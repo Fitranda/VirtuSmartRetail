7a69f6246e06dcd534ddad02574127e87f0eb4db
Fitranda Ramadhana <68302339+Fitranda@users.noreply.github.com>
2024-12-19 04:24:12 +0700

    Revert "Update web.php"
    This reverts commit 24ab8b7e1a84fedbaa1480c7da6414e27bbac098.

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use App\Http\Controllers\LaporanPenjualanController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\StokRequestController;
 use App\Http\Controllers\PembelianController;
+use App\Http\Controllers\JurnalController;
 
 
 
@@ -128,7 +129,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/barang', [PembelianController::class, 'pilihproduk'])->name('produk');
     Route::post('/savepembelian', [PembelianController::class, 'beli'])->name('savebeli');
 
+    Route::resource('jurnals', JurnalController::class);
+
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
     Route::get('/laporan', [LaporanPenjualanController::class, 'index'])->name('laporan.index');
 });
+
+
+
